613316be09eb6b4d98f93d857cbb0cc97a9ae630
matheusfrancisco <matheusmachadoufsc@gmail.com>
2024-01-20 20:03:42 -0300

    improve v1

--- src/stackfn/core.clj
+++ src/stackfn/core.clj
@@ -6,74 +6,44 @@
 
 (defmacro defstackfn [fn-name args & body]
   `(defn ~fn-name [& args#]
-     (-> (lang/read-eval '(~@body)
-                         (assoc {:bindings {}
-                                 :state []}
-                                :bindings (zipmap '~args args#)))
-         :state
-         last)))
+     (try
+       (-> (lang/read-eval '(~@body)
+                           (assoc {:bindings {}
+                                   :state []}
+                                  :bindings (zipmap '~args args#)))
+           :state
+           last)
+       (catch Exception e#
+         (let [s#  (first (.getStackTrace e#))]
+           (prn (.getLineNumber s#)))))))
 
 #_(defstackfn fa [!a  !b]
     !a
     !b
     (invoke>  * 2))
 
-; (err) Execution error (IllegalArgumentException) at stackfn.lang/eval60840$fn$G (REPL:60).
-; (err) No implementation of method: :eval> of protocol: #'stackfn.lang/IEval found for class: nil
-(defstackfn fb [!a  !b]
+(defstackfn f [!a !b !c]
   !a
   !b
-  (invoke>  * 2)
+  (invoke> + 2)
   !v1+
+  !c
+  !c
+  <pop>
+  2
+  (invoke> * 2)
+  !v2+
+  (invoke> = 2)
   (if>
    !v1
-   !v1
+   !v2
    (invoke> - 2)
-   #_else>
+   else>
    "false!!"
    (invoke> println 1)
    <pop>
    !v1
-   !v1
+   !v2
    (invoke> * 2)))
 
-(fb 1 2)
-
-#_(defstackfn f [!a !b !c]
-    !a ; 1
-    !b ; 2
-    (invoke> + 2) ;3
-    !v1+
-    !c
-    !c
-    <pop>
-    2
-    (invoke> * 2)
-    !v2+
-    (invoke> = 2)
-    (if>
-     !v1
-     !v2
-     (invoke> - 2)
-     else>
-     "false!!"
-     (invoke> println 1)
-     <pop>
-     !v1
-     !v2
-     (invoke> * 2)))
-
-#_(f 1 2 4)
-
-#_(comment
-    (defstackfn fa [!a  !b]
-      !a
-      !b
-      (invoke>  * 2))
-
-    (fa 2 4)
-
-;
-    )
-
-; print false and returns 24
+(f 1 2 4)
